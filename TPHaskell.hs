30b38a94a34c432fec6d8ec6ec09c3e842ce738f
Paulo Luiz <paulolsouza19@gmail.com>
2025-08-24 23:51:45 -0300

    implemented function to generate all possible expressions from a list of numbers

--- TPHaskell.hs
+++ TPHaskell.hs
@@ -1,11 +1,15 @@
+module TPHaskell where
+
+-- (a) Tipo de dado para representar expressões
 data Expr = Sum Expr Expr
           | Mult Expr Expr
           | Sub Expr Expr
           | Div Expr Expr
           | Lit Int
           | Var String
-          deriving show
+          deriving Show
 
+-- (b) Tipo de dados para representar o resultado da expressão
 data Result = Ok Int 
             | Error String 
             deriving Show
@@ -22,6 +26,7 @@ safeDiv :: Int -> Int -> Result
 safeDiv _ 0 = Error "Divisão por zero"
 safeDiv n1 n2 = Ok (n1 `div` n2)
 
+-- Função para avaliar uma expressão
 eval :: Expr -> Result
 eval (Lit n) = Ok n
 eval (Var _) = Error "Variáveis não suportadas nesta fase"
@@ -29,3 +34,21 @@ eval (Sum e1 e2) = applyOperator (wrap (+)) (eval e1) (eval e2)
 eval (Mult e1 e2) = applyOperator (wrap (*)) (eval e1) (eval e2)
 eval (Sub e1 e2) = applyOperator (wrap (-)) (eval e1) (eval e2)
 eval (Div e1 e2) = applyOperator safeDiv (eval e1) (eval e2)
+
+-- Função para gerar todas as expressões possíveis a partir de uma lista de números
+expressions :: [Int] -> [Expr]
+expressions [] = []
+expressions [n] = [Lit n]
+expressions ns = 
+  [ op le re
+  | (es, ds) <- splits ns 
+  , le <- expressions es 
+  , re <- expressions ds
+  , op <- [Sum, Mult, Sub, Div]
+  ]
+
+-- Função para dividir uma lista em duas partes não vazias
+splits :: [a] -> [([a], [a])]
+splits [] = []
+splits [_] = []
+splits (x:xs) = ([x], xs) : [ (x:xs, ds) | (es, ds) <- splits xs ]
